ad44d7f2e558db4f742b5f235d3e67156a0bb8bb
nsontung <sontung.nguyen@gmail.com>
2016-10-07 11:00:04 +0700

    Add new worker scala version
    Signed-off-by: nsontung <sontung.nguyen@gmail.com>

---
+++ workersc/src/com/mxhgt/sc/main/Main.scala
@@ -0,0 +1,15 @@
+package com.mxhgt.sc.main
+
+/**
+  * Created by tungns on 10/7/16.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Worker is starting...")
+
+
+
+  }
+
+
+}
